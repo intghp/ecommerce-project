5e6970466304d469844bd1091171656efc520c6a
intghp <gustavohlucenapaiva1@gmail.com>
2025-07-07 20:31:22 -0300

    views and urls

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {"message": "User created successfully"},
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
